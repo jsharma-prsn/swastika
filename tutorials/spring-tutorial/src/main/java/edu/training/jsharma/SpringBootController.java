7636c4c1936a6a9aac8a707bba7b103cd162796a
Jitendra Sharma <jitendrasharma42@gmail.com>
2019-09-02 02:53:01 +0530

    Update pom.xml, SpringBootApplication.java

---
+++ tutorials/spring-tutorial/src/main/java/edu/training/jsharma/SpringBootController.java
@@ -0,0 +1,17 @@
+package edu.training.jsharma;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootController {
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return counter.incrementAndGet() + String.format(template, name);
+  }
+}

74ae0fd89de39c82277426ca34c6a6e2c0aca8b4
Jitendra Sharma <jitendrasharma42@gmail.com>
2019-09-02 01:34:26 +0530

    Spring tutorial

---
+++ tutorials/spring-tutorial/src/main/java/com/training/spring/restservice/SpringBootController.java
@@ -0,0 +1,17 @@
+package com.training.spring.restservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class SpringBootController {
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return counter.incrementAndGet() + String.format(template, name);
+  }
+}

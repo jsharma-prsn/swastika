c9ce950b53d8c92eee72dea26f123c24f76ec2eb
Jitendra Sharma <jitendrasharma42@gmail.com>
2019-09-24 19:51:47 +0530

    Update ApacheBeamKafkaIntegration.java

--- tutorials/apache-beam-tutorial/src/main/java/edu/jsharma/training/ApacheBeamKafkaIntegration.java
+++ tutorials/apache-beam-tutorial/src/main/java/edu/jsharma/training/ApacheBeamKafkaIntegration.java
@@ -1,15 +1,22 @@
 package edu.jsharma.training;
 
-import org.apache.beam.repackaged.beam_runners_core_construction_java.com.google.common.collect.ImmutableMap;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.Compression;
+import org.apache.beam.sdk.io.FileIO;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.fs.MatchResult.Metadata;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.joda.time.Instant;
+import com.google.common.collect.ImmutableMap;
 
 public class ApacheBeamKafkaIntegration {
   public static void main(String[] args) {
@@ -18,6 +25,22 @@ public class ApacheBeamKafkaIntegration {
 
     Pipeline p = Pipeline.create(options);
 
+    PCollection<Metadata> files =
+        p.apply("Checking file pattern", FileIO.match().filepattern("input/input*"));
+
+    files
+        .apply("Checking file matches", FileIO.readMatches().withCompression(Compression.GZIP))
+        .apply("Read CSV Files", TextIO.readFiles())
+        .apply(
+            KafkaIO.<Void, String>write()
+                .withBootstrapServers("localhost:9092")
+                .withTopic("USER_TEST")
+                .withInputTimestamp()
+                .withPublishTimestampFunction((elem, elemTs) -> new Instant())
+                .withValueSerializer(StringSerializer.class)
+                .values());
+
+    // Reading from kafka
     p.apply(
             KafkaIO.<Long, String>read()
                 .withBootstrapServers("localhost:9092")
@@ -25,6 +48,7 @@ public class ApacheBeamKafkaIntegration {
                     "USER_TEST") // use withTopics(List<String>) to read from multiple topics.
                 .withKeyDeserializer(LongDeserializer.class)
                 .withValueDeserializer(StringDeserializer.class)
+
                 // .withStartReadTime(new Instant())
 
                 // Above four are required configuration. returns PCollection<KafkaRecord<Long,
